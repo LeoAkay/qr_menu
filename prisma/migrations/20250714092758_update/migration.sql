689f670903c1193032a684d6afe3585288d1e131
LeoAkay <69201536+LeoAkay@users.noreply.github.com>
2025-07-14 12:53:11 +0300

    db

---
+++ prisma/migrations/20250714092758_update/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `UpdatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "CreatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "UpdatedAt" TIMESTAMP(3) NOT NULL;
